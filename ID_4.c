dfa800788f489ee35eca6624a3c72f7ea883b552
Vinicius Souza <vineasouza33@hotmail.com>
2018-05-06 01:56:21 -0300

    comentando exercicio 4

--- ID_4.c
+++ ID_4.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//funcao que inverte o numero e retorna para uma variavel auxiliar
 int inverte_retorna(int numero) {
    int aux = 0;
 
@@ -16,15 +17,18 @@ int main(int argc, char const *argv[]) {
     int numero = 0;
     int maior = 0;
 
+    //multilica os valores no looping
     for (int i = 100; i < 999; i++) {
         for(int j = 100; j < 999; j++) {
             numero = i * j;
 
             int aux = inverte_retorna(numero);
 
+            //verifica se o valor é igual ao inverso do valor passado pelo looping
             if (numero == aux) {
                 printf("Numero palindromo -> %d\n", numero);
 
+                //guarda o maior valor
                 if (numero > maior) {
                 maior = numero;
                 }
