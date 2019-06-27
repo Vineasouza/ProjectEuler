5482ec9ea06159c5d93428607095d89aa30796eb
Vinicius Souza <vineasouza33@hotmail.com>
2019-06-27 01:53:27 -0300

    adicionando exercicio 7

---
+++ ID_7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+double primo() {
+    double resp = 1, primo = 5;
+    while(resp <= 10 001) {
+        primo+=2
+        resp++;
+    }
+    return primo;
+}
+
+int main(int argc, char const *argv[]) {
+    double resposta = 0;
+
+    resposta = primo();
+
+    return 0;
+}
