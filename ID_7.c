c2efa7d47ebd2b70f354a8f32a6cb1a063a60b8f
Vinicius Souza <vineasouza33@hotmail.com>
2019-06-27 15:20:27 -0300

    adicionando algoritimo certo exercicio 7

--- ID_7.c
+++ ID_7.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
+#include <math.h>
+
+int isprime(long num) {
+    long i;
+    for(i = 2; i<= sqrt(num); i++) {
+        if(num % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+long primeNumer(int num) {
+    long i;
+    int counter = 0;
+
+    for(i = 2;; i++) {
+        if(isprime(i)) {
+            counter++;
+        }
+        if(counter == num) {
+            break;
+        }
+    }
+
+    return i;
+}
 
 int primo() {
     int resp = 1, primo = 5;
@@ -10,11 +38,9 @@ int primo() {
 }
 
 int main(int argc, char const *argv[]) {
-    int resposta = 0;
+    int i = 10001;
 
-    resposta = primo();
-
-    printf("> %d", resposta);
+    printf("> %ld", primeNumer(i));
 
     return 0;
 }
